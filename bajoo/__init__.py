d8c2cb2ae4755c8f97e6b8630f8f1076a22cd418
Kévin Bernard-Allies <kbernard-allies@bajoo.fr>
2015-05-18 14:31:06 +0200

    Add wxPython as a dependency. Python3 will use the "Phoenix" version (still in dev) and Python2 the "classic" version.

--- bajoo/__init__.py
+++ bajoo/__init__.py
@@ -2,9 +2,15 @@
 
 from .__version__ import __version__  # noqa
 
+import wx
+
 
 def main():
     """Entry point of the Bajoo client."""
+    app = wx.App()
+    frame = wx.Frame(None, wx.ID_ANY, "Hello World")
+    frame.Show(True)
+    app.MainLoop()
     pass
 
 
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 
 from setuptools import find_packages, setup
 from setuptools.command.test import test as TestCommand
+from setuptools.command.install import install as InstallCommand
 import sys
 
 
@@ -12,6 +13,16 @@ exec(open('bajoo/__version__.py').read())
 
 with open('README.rst') as readme_file:
     long_description = readme_file.read()
+
+
+requirements = [
+    'requests>=2.6.0',
+    'futures>=2.2.0'
+]
+if sys.version_info[0] is 3:  # Python3 only
+    requirements += [
+        'wxpython>=Phoenix-3.0.0.dev,<Phoenix-9999'
+    ]
 
 
 class Tox(TestCommand):
@@ -32,6 +43,30 @@ class Tox(TestCommand):
         import shlex
         errno = tox.cmdline(args=shlex.split(self.tox_args))
         sys.exit(errno)
+
+
+class Install(InstallCommand):
+    """Overrides the install command to check the presence of wxPython."""
+
+    def run(self):
+        # Note(Kevin): On Python 2, Bajoo should uses the stable 'Classic'
+        # version. This version must be installed manually by the user
+        # (usually the package manager of the distribution).
+        # On python 3, Bajoo uses the 'Phoenix' version of wxPython.
+        # At this time, the Phoenix version is not stable yet, and only daily
+        # snapshots are available.
+
+        if sys.version_info[0] is 2:
+            try:
+                import wx  # noqa
+            except ImportError:
+                print("""\
+Bajoo depends on the library wxPython. This library is not available in the
+Python Package Index (pypi), and so can't be automatically installed.
+On Linux, you can install it from your distribution's package repositories.
+On Windows, you can download it from http://wxpython.org/download.php""")
+                raise Exception('wxPython not found.')
+        InstallCommand.run(self)
 
 
 setup(
@@ -56,17 +91,18 @@ setup(
     ],
     keywords="bajoo storage cloud file-sharing",
     packages=find_packages(),
-    install_requires=['requests>=2.6.0',
-                      'futures>=2.2.0'],
+    install_requires=requirements,
     tests_require=['tox'],
     include_package_data=True,
     package_data={},
+    dependency_links=['http://wxpython.org/Phoenix/snapshot-builds/'],
     entry_points={
         "console_scripts": [
             "bajoo=bajoo:main"
         ]
     },
     cmdclass={
+        'install': Install,
         'test': Tox
     },
     zip_safe=True
