6888203cc69a6603524eeb3b94b1178655c7ca8b
Kévin Bernard-Allies <kevin.bernard.allies@gmail.com>
2015-05-19 17:31:19 +0200

    Start log and config modules in main()

--- bajoo/__init__.py
+++ bajoo/__init__.py
@@ -3,15 +3,23 @@
 from .__version__ import __version__  # noqa
 
 import wx
+from .common import log
+from .common import config
 
 
 def main():
     """Entry point of the Bajoo client."""
+
+    # Start log and load config
+    log.init()
+    config.load()
+    log.set_debug_mode(config.get('debug_mode'))
+    log.set_logs_level(config.get('log_levels'))
+
     app = wx.App()
     frame = wx.Frame(None, wx.ID_ANY, "Hello World")
     frame.Show(True)
     app.MainLoop()
-    pass
 
 
 if __name__ == "__main__":
