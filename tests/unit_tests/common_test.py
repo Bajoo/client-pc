be813811746c0530568e21fb55d30509bbe1e076
Kévin Bernard-Allies <kbernard-allies@bajoo.fr>
2015-06-01 11:16:29 +0200

    Add more tests for bajoo.common.future

--- tests/unit_tests/common_test.py
+++ tests/unit_tests/common_test.py
@@ -1,13 +1,22 @@
 # -*- coding: utf8 -*-
 
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import CancelledError, ThreadPoolExecutor
 from threading import Lock
+import pytest
 
-from bajoo.common.future import then
+from bajoo.common.future import Future, patch, patch_dec, then, main
 
 
 class TestFuture(object):
     """Test of the bajoo.common.future module"""
+
+    def test_main(self):
+        """Try the small main() presentation function of the module.
+
+        This test just ensures that the main() don't throaw an exception.
+        The execution may or may not be correct.
+        """
+        main()
 
     def test_then_before_run(self):
         """Test to chain a callback using then() before start.
@@ -72,3 +81,85 @@ class TestFuture(object):
             future_second_step = then(future_first_step, _callback)
 
             assert future_second_step.result(1) is 112
+
+    def test_then_on_failing_future(self):
+        """Chain a callback using then() to a future raising an exception"""
+        def _task():
+            raise NotImplementedError
+
+        def _callback(__):
+            assert not 'This should not be executed.'
+
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            future = executor.submit(_task)
+            second_future = then(future, _callback)
+
+        with pytest.raises(NotImplementedError):
+            second_future.result()
+
+    def test_then_on_cancelled_future(self):
+        """Using then() ,on a cancelled future will raises an exception."""
+        wait_lock = Lock()
+
+        # This task will block until the lock is available.
+        def _task_wait_lock(lock):
+            lock.acquire()
+            lock.release()
+
+        def _callback(arg):
+            raise Exception('This should never be called.')
+
+        wait_lock.acquire()
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            executor.submit(_task_wait_lock, wait_lock)
+
+            future = executor.submit(lambda: 23)
+            assert future.cancel() is True
+            wait_lock.release()
+            with pytest.raises(CancelledError):
+                then(future, _callback)
+
+    def test_patched_future(self):
+        """Patch the future and chain the future with a callback."""
+
+        def _callback(arg):
+            assert arg is 82
+            return arg // 2
+
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            future_first_step = patch(executor.submit(lambda: 82))
+            future_second_step = future_first_step.then(_callback)
+
+            assert future_second_step.result(1) is 41
+
+    def test_patch_decorator(self):
+        """Patch a function who returns a future and test the result."""
+
+        @patch_dec
+        def async_method():
+            with ThreadPoolExecutor(max_workers=1) as executor:
+                return executor.submit(lambda: 33)
+
+        def _callback(arg):
+            assert arg is 33
+            return arg * 3
+
+        future_first_step = async_method()
+        future_second_step = future_first_step.then(_callback)
+        assert future_second_step.result(1) is 99
+
+    def test_resolve_value(self):
+        """Wrap a value into a future using Future.resolve()."""
+        future = Future.resolve('xyz')
+        assert not future.cancelled()
+        assert not future.running()
+        assert future.done()
+        assert future.result() is 'xyz'
+
+    def test_resolve_future(self):
+        """Use Future.resolve() on an object who is already a future."""
+
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            f = executor.submit(lambda: 33)
+            future = Future.resolve(f)
+            assert future.result(1) is 33
