4d4d625062cc0baf9c88a8e5d27f088ddd20ca2a
Kévin Bernard-Allies <kbernard-allies@bajoo.fr>
2015-06-16 14:57:41 +0200

    Implement proxy form controller.

--- bajoo/common/config.py
+++ bajoo/common/config.py
@@ -27,7 +27,15 @@ _default_config = {
     'lang': {'type': str, 'default': None},
     # TODO: set default debug_mode to False for stable release
     'debug_mode': {'type': bool, 'default': True},
-    'log_levels': {'type': dict, 'default': {}}
+    'log_levels': {'type': dict, 'default': {}},
+    # Can be "no_proxy", "system_settings" or "manual_settings"
+    'proxy_mode': {'type': str, 'default': 'system_settings'},
+    # can be "SOCKS4", "SOCKS5" or "HTTP".
+    'proxy_type': {'type': str, 'default': 'HTTP'},
+    'proxy_url': {'type': str, 'default': None},
+    'proxy_port': {'type': int, 'default': None},
+    'proxy_user': {'type': str, 'default': None},
+    'proxy_password': {'type': str, 'default': None}
 }
 
 # Actual config parser
--- bajoo/gui/form/proxy_form.py
+++ bajoo/gui/form/proxy_form.py
@@ -2,6 +2,7 @@
 
 import wx
 
+from ...common import config
 from ...common.i18n import N_
 from ..base_view import BaseView
 from . import BaseForm
@@ -10,76 +11,146 @@ from . import BaseForm
 class ProxyForm(BaseForm):
     """Form of the proxy configuration.
 
-    This form don't contains any submit button.
+    This form contains the following named window:
+    * system_settings (wx.Radio)
+    * no_proxy (wx.Radio)
+    * manual_settings (wx.Radio)
+    * proxy_type (wx.Choice): selection can be 0 (HTTP), 1 (SOCKS4) or
+        2 (SOCKS5).
+    * server_uri (wx.TextCtrl)
+    * server_port (wx.TextCtrl)
+    * use_auth (wx.Checkbox): If checked, the "username" and "password" fields
+        must be used has authentication credentials.
+    * username (wx.TextCtrl)
+    * password (wx.TextCtrl)
+
+    This form don't contains any submit button. The submission of te form must
+    be done outside (either manually, or by a parent form).
     """
     def __init__(self, parent, **kwargs):
         BaseForm.__init__(self, parent, **kwargs)
         self._view = ProxyFormView(self)
-        # TODO: implement ...
+
+        self._view.create_children()
+        self._view.create_layout()
+        self.populate()
+        self.apply_field_constraints()
+
+        self.Bind(wx.EVT_RADIOBUTTON, self.apply_field_constraints)
+        self.Bind(wx.EVT_CHECKBOX, self.apply_field_constraints,
+                  self.FindWindowByName('use_auth'))
+
+    def populate(self):
+        mode = config.get('proxy_mode')
+        proxy_type = config.get('proxy_type')
+        url = config.get('proxy_url') or ''
+        port = config.get('proxy_port') or ''
+        user = config.get('proxy_user') or ''
+        password = config.get('proxy_password') or ''
+
+        mode_radio = self.FindWindowByName(mode)
+        if mode_radio:
+            mode_radio.SetValue(True)
+        else:
+            self.FindWindowByName('system_settings').SetValue(True)
+
+        config2type = {
+            'HTTP': 0,
+            'SOCKS4': 1,
+            'SOCKS5': 2,
+        }
+        selection = config2type.get(proxy_type, 0)
+        self.FindWindowByName('proxy_type').SetSelection(selection)
+
+        self.FindWindowByName('server_uri').SetValue(url)
+        self.FindWindowByName('server_port').SetValue(port)
+        self.FindWindowByName('username').SetValue(user)
+        self.FindWindowByName('password').SetValue(password)
+
+    def apply_field_constraints(self, _evt=None):
+        """Set the form in a coherent state by applying fields constraints.
+
+        Note: it takes an ignored argument (optional), so it can be used has
+        an event handler.
+        """
+
+        is_manual_config = self.FindWindowByName('manual_settings').GetValue()
+        use_auth = self.FindWindowByName('use_auth').GetValue()
+
+        for name in ('proxy_type', 'server_uri', 'server_port', 'use_auth'):
+            self.FindWindowByName(name).Enable(is_manual_config)
+
+        for name in ('username', 'password'):
+            self.FindWindowByName(name).Enable(is_manual_config and use_auth)
 
 
 class ProxyFormView(BaseView):
     """View of the ProxyForm"""
 
-    def __init__(self, proxy_form):
-        BaseView.__init__(self, proxy_form)
+    def create_children(self):
+        """Create all named children of proxy form."""
 
-        box = wx.StaticBox(proxy_form)
-        self.register_i18n(proxy_form.SetLabel, N_('Proxy'))
-
-        # Proxy type
-        auto_radio = wx.RadioButton(proxy_form, style=wx.RB_GROUP,
-                                    name='auto_config')
-        self.register_i18n(
-            auto_radio.SetLabel,
-            N_('Automatic detection of proxy settings for this network.'))
-        system_radio = wx.RadioButton(proxy_form, name='system_config')
+        system_radio = wx.RadioButton(self.window, name='system_settings',
+                                      style=wx.RB_GROUP)
         self.register_i18n(system_radio.SetLabel, N_('System settings'))
-        no_radio = wx.RadioButton(proxy_form, name='no_config')
+        no_radio = wx.RadioButton(self.window, name='no_proxy')
         self.register_i18n(no_radio.SetLabel, N_('Do not use proxy'))
-        manual_radio = wx.RadioButton(proxy_form, name='manual_config')
+        manual_radio = wx.RadioButton(self.window, name='manual_settings')
         self.register_i18n(manual_radio.SetLabel, N_('Manual settings'))
 
-        proxy_type_txt = wx.StaticText(proxy_form)
-        self.register_i18n(proxy_type_txt.SetLabel, N_('Proxy type:'))
-        proxy_type_choice = wx.Choice(proxy_form, name='proxy_type',
-                                      choices=["HTTP", "SOCKS4", "SOCKS5"])
-        proxy_type_choice.SetSelection(0)
+        wx.Choice(self.window, name='proxy_type',
+                  choices=["HTTP", "SOCKS4", "SOCKS5"])
 
-        server_input = wx.TextCtrl(proxy_form, name='server_uri')
-        port_input = wx.TextCtrl(proxy_form, name='server_port')
-        auth_box = wx.CheckBox(proxy_form, name='use_auth')
+        wx.TextCtrl(self.window, name='server_uri')
+        wx.TextCtrl(self.window, name='server_port')
+
+        auth_box = wx.CheckBox(self.window, name='use_auth')
         self.register_i18n(auth_box.SetLabel,
                            N_('The server requires an authentication'))
+        wx.TextCtrl(self.window, name='username')
+        wx.TextCtrl(self.window, style=wx.TE_PASSWORD, name='password')
 
-        username_input = wx.TextCtrl(proxy_form, name='username')
-        password_input = wx.TextCtrl(proxy_form, style=wx.TE_PASSWORD,
-                                     name='password')
+    def create_layout(self):
+        """Create appropriate layout and static text for proxy form."""
+
+        proxy_type_txt = wx.StaticText(self.window)
+        self.register_i18n(proxy_type_txt.SetLabel, N_('Proxy type:'))
 
         sizer_radio = wx.BoxSizer(wx.VERTICAL)
-        sizer_radio.AddMany([auto_radio, system_radio, no_radio, manual_radio])
+        sizer_radio.AddMany([
+            self.window.FindWindowByName('system_settings'),
+            self.window.FindWindowByName('no_proxy'),
+            self.window.FindWindowByName('manual_settings')
+        ])
 
+        # Line "[ server_uri ] : [ server_port ]"
         uri_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        uri_sizer.Add(server_input, proportion=5, flag=wx.ALIGN_CENTER)
-        uri_sizer.Add(wx.StaticText(proxy_form, label=':'),
-                      flag=wx.ALIGN_CENTER | wx.RIGHT | wx.LEFT,
-                      border=10)
-        uri_sizer.Add(port_input, proportion=1, flag=wx.ALIGN_CENTER)
+        uri_sizer.Add(self.window.FindWindowByName('server_uri'),
+                      proportion=5, flag=wx.ALIGN_CENTER)
+        uri_sizer.Add(wx.StaticText(self.window, label=':'),
+                      flag=wx.ALIGN_CENTER | wx.RIGHT | wx.LEFT, border=10)
+        uri_sizer.Add(self.window.FindWindowByName('server_port'),
+                      proportion=1, flag=wx.ALIGN_CENTER)
 
         s = self.make_sizer(wx.VERTICAL, [
             sizer_radio,
-            self.make_sizer(wx.HORIZONTAL, [proxy_type_txt, proxy_type_choice],
-                            outside_border=False, flag=wx.ALIGN_CENTER),
+            self.make_sizer(wx.HORIZONTAL, [
+                proxy_type_txt,
+                self.window.FindWindowByName('proxy_type')
+            ], outside_border=False, flag=wx.ALIGN_CENTER),
             uri_sizer,
-            auth_box,
-            self.make_sizer(wx.HORIZONTAL, [username_input, password_input],
-                            outside_border=False, proportion=1)
+            self.window.FindWindowByName('use_auth'),
+            self.make_sizer(wx.HORIZONTAL, [
+                self.window.FindWindowByName('username'),
+                self.window.FindWindowByName('password')
+            ], outside_border=False, proportion=1)
         ])
 
+        box = wx.StaticBox(self.window)
+        self.register_i18n(self.window.SetLabel, N_('Proxy'))
         sizer = wx.StaticBoxSizer(box, wx.VERTICAL)
         sizer.Add(s, flag=wx.EXPAND)
-
-        proxy_form.SetSizer(sizer)
+        self.window.SetSizer(sizer)
 
 
 def main():
