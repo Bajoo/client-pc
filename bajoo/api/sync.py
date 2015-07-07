0c5453aa84341afb084475daaf85e7e839c44c3f
Kévin Bernard-Allies <kbernard-allies@bajoo.fr>
2015-07-07 17:57:58 +0200

    Add class DynamicContainerList (keep sync local and remote container list)

--- bajoo/api/sync.py
+++ bajoo/api/sync.py
@@ -15,6 +15,7 @@ class PeriodicTask(object):
         self._task = task
         self._args = args
         self._timer = None
+        self._canceled = False
 
     def _exec_task(self, *args, **kwargs):
         try:
@@ -24,19 +25,22 @@ class PeriodicTask(object):
                               self._task)
         self._timer = Timer(self._delay, self._exec_task,
                             args=self._args)
-        self._timer.start()
+        if not self._canceled:
+            self._timer.start()
 
     def start(self):
-        _logger.debug('Start periodic task %s', self._exec_task)
+        _logger.debug('Start periodic task %s', self._task)
         self._timer = Timer(0, self._exec_task, args=self._args)
         self._timer.start()
 
     def stop(self):
-        _logger.debug('Stop periodic task %s', self._exec_task)
+        _logger.debug('Stop periodic task %s', self._task)
+        self._canceled = True
         self._timer.cancel()
 
 
 def container_list_updater(session, on_added_containers, on_removed_containers,
+                           on_unchanged_containers=None,
                            last_known_list=None, check_period=600):
     """Detect changes in the user's container list.
 
@@ -51,6 +55,9 @@ def container_list_updater(session, on_added_containers, on_removed_containers,
         on_removed_containers (callable): called with the list of container's
             id who've been removed in parameters, each time a container is
                 removed.
+        on_unchanged_containers (callable, optional): If set, it will be called
+            on the first iteration, with the containers who hasn't been
+            changed.
         last_known_list (list of str, optional): list of container's ids
             already known. If not set, all containers will be considered as
             'new'.
@@ -60,22 +67,28 @@ def container_list_updater(session, on_added_containers, on_removed_containers,
             stopped with `stop()`
     """
 
-    def update_list(last_known_list):
+    def update_list(last_known_list, on_unchanged_containers=None):
         f = Container.list(session)
         container_list = f.result()
 
         added_list = [c for c in container_list if c.id not in last_known_list]
-        new_id_list = [c.id for c in container_list]
+        id_list = [c.id for c in container_list]
         removed_id_list = [id for id in last_known_list
-                           if id not in new_id_list]
+                           if id not in id_list]
 
+        if on_unchanged_containers:
+            unchanged_list = [c for c in container_list
+                              if c.id in last_known_list]
+            if unchanged_list:
+                on_unchanged_containers(unchanged_list)
         if added_list:
             on_added_containers(added_list)
         if removed_id_list:
             on_removed_containers(removed_id_list)
-        return [new_id_list]
+        return [id_list]
 
-    return PeriodicTask(check_period, update_list, last_known_list or [])
+    return PeriodicTask(check_period, update_list, last_known_list or [],
+                        on_unchanged_containers)
 
 
 def main():
---
+++ bajoo/dynamic_container_list.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+
+import json
+import logging
+from threading import Lock as Lock
+import os
+import errno
+
+from .common.path import get_data_dir
+from .api.sync import container_list_updater
+
+_logger = logging.getLogger(__name__)
+
+
+class DynamicContainerList(object):
+    """Keep sync the local and remote lists of containers.
+
+    The class contains the up-to-date list of containers, and can be used to
+    list and see the container's status.
+
+    It will detect differences between the last sync of containers list, check
+    path for already known containers et creates new folders for new
+    containers.
+    It will also prevent the user about added or removed containers, as well
+    as error path.
+
+    All local, know containers are registered in the file
+    `container_list.json`
+
+    The file has the following format:
+    [{
+      'id': '123456',
+      'name': 'My Container'
+      'path': '/home/user/Bajoo/my_container/'
+    },
+    { ... }]
+
+    The elements in self._local_list contains one more attribute: 'container'.
+    'container' is set as soon as possible. If it doesn't exists, the element
+    should be considered being loaded.
+    """
+
+    def __init__(self, session):
+        self._list_path = os.path.join(get_data_dir(), 'container_list.json')
+        self._local_list = []
+        self._list_lock = Lock()
+
+        try:
+            with open(self._list_path) as list_file:
+                self._local_list = json.load(list_file)
+        except IOError as e:
+            if e.errno is errno.ENOENT:
+                _logger.debug('Container list file not found.')
+            else:
+                _logger.warning('Unable to open the container list file:',
+                                exc_info=True)
+        except ValueError:
+            _logger.warning('The container list file is not valid:',
+                            exc_info=True)
+
+        local_id_list = [c['id'] for c in self._local_list]
+
+        self._updater = container_list_updater(session,
+                                               self._on_added_containers,
+                                               self._on_removed_containers,
+                                               self._init_containers,
+                                               local_id_list)
+        self._updater.start()
+
+    def _save_local_list(self):
+        """Save the local list file."""
+        try:
+            with open(self._list_path, 'wb') as list_file, self._list_lock:
+                local_list = [{'id': c['id'],
+                               'name': c['name'],
+                               'path': c['path']} for c in self._local_list]
+                json.dump(local_list, list_file)
+        except IOError:
+            _logger.warning("The container list file can't be saved:",
+                            exc_info=True)
+
+    def _init_containers(self, containers_list):
+        _logger.debug('Container(s) loaded: %s', containers_list)
+
+        with self._list_lock:
+            for item in self._local_list:
+                for c in containers_list:
+                    if c.id == item['id']:
+                        item['container'] = c
+                        break
+
+        # TODO: find and check the path of each container
+        # If the path is not valid, or if there isn't an index file in the
+        # folder, we must inform the user and set the status to ERROR.
+        # TODO: start each container
+
+    def _on_added_containers(self, added_containers):
+        _logger.info('New container(s) detected: %s', added_containers)
+
+        # TODO: inform the USER !
+
+        with self._list_lock:
+            for c in added_containers:
+                self._local_list.append({
+                    'id': c.id,
+                    'name': c.name,
+                    'path': None,
+                    'container': c
+                })
+        self._save_local_list()
+
+        # TODO: associate folders to the containers
+        # Generate path for the container's name.
+        # If a path is taken, generate a new one, unless the index file match.
+        # TODO: start the containers.
+
+    def _on_removed_containers(self, removed_containers):
+        _logger.info('container(s) removed: %s', removed_containers)
+
+        with self._list_lock:
+            for container in removed_containers:
+                to_remove = [c for c in self._local_list
+                             if c['id'] == container.id]
+                # TODO: inform the USER !
+                for c in to_remove:
+                    self._local_list.remove(c)
+
+        # TODO: properly stop the containers (if started).
+
+        self._save_local_list()
+
+    def stop(self):
+        self._updater.stop()
+
+    def get_list(self):
+        """returns the list of containers.
+
+        Returns:
+            list of dict: each dict represent a container. It contains:
+             - id (str)
+             - name (str)
+             - path (str): corresponding path on the disk.
+             - container (bajoo.api.Container): if set, container instance.
+             - status (str)
+
+         Note: the returned container instance is not a copy but a reference.
+         It will not be modified directly by the dynamic list.
+        """
+        with self._list_lock:
+            # Note: item.container are copied by reference.
+            return [dict(c) for c in self._local_list]
+
+
+def main():
+    import time
+    from .api.session import Session
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    session = Session.create_session('stran+20@bajoo.fr',
+                                     'stran+20@bajoo.fr').result()
+    dyn_list = DynamicContainerList(session)
+    try:
+        while True:
+            time.sleep(0.3)
+    except KeyboardInterrupt:
+        dyn_list.stop()
+
+
+if __name__ == "__main__":
+    main()
