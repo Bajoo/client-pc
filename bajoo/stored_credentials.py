fdcf54c1172c8fe021f1037170ae3db5a9d1d25f
Kévin Bernard-Allies <kbernard-allies@bajoo.fr>
2015-07-21 14:42:58 +0200

    Save and load refresh_token

--- bajoo/stored_credentials.py
+++ bajoo/stored_credentials.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 """Functions to load and store user credentials persistently."""
 
+import errno
+import json
 import logging
+import os.path
+
+from .common.path import get_config_dir
 
 _logger = logging.getLogger(__name__)
 
@@ -14,11 +19,33 @@ def load():
             (None, None) is returned.
     """
     username, refresh_token = None, None
-    # TODO: to implement
+    token_path = os.path.join(get_config_dir(), 'token')
+    file_content = None
+
+    try:
+        with open(token_path, 'r') as token_file:
+            file_content = json.load(token_file)
+    except (IOError, OSError) as error:
+        if error.errno == errno.ENOENT:
+            _logger.info('refresh_token file not found.')
+        else:
+            _logger.warning('Unable to load refresh_token file:',
+                            exc_info=True)
+        return None, None
+    except ValueError:
+        _logger.warning('Unable to load refresh_token file:', exc_info=True)
+        return None, None
+
+    try:
+        username = file_content[0]
+        refresh_token = file_content[1]
+    except (IndexError, KeyError):
+        _logger.warning('Error when loading refresh_token file:',
+                        exc_info=True)
+        return username, refresh_token
 
     _logger.debug('credentials loaded: username="%s" token %s' %
                   (username, 'found' if refresh_token else 'not found'))
-
     return username, refresh_token
 
 
@@ -29,4 +56,10 @@ def save(username, refresh_token=None):
         refresh_token (str, optional): OAuth2 refresh token.
     """
     _logger.debug('Credentials saved on disk.')
-    # TODO: to implement
+
+    token_path = os.path.join(get_config_dir(), 'token')
+    try:
+        with open(token_path, 'w') as token_file:
+            json.dump([username, refresh_token], token_file)
+    except (IOError, OSError, ValueError):
+        _logger.warning('Unable to save refresh_token file:', exc_info=True)
