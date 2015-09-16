f215c7412442658d5d24aade2ca7e0e0897e3200
Kévin Bernard-Allies <kbernard-allies@bajoo.fr>
2015-09-16 16:52:04 +0200

    Add unit test for xor() function, to check unicode entries are accepted.

--- tests/unit_tests/common_test.py
+++ tests/unit_tests/common_test.py
@@ -1,10 +1,12 @@
 # -*- coding: utf8 -*-
 
 from concurrent.futures import CancelledError, ThreadPoolExecutor
+from itertools import product
 from threading import Lock
 import pytest
 
 from bajoo.common.future import Future, patch, patch_dec, then, main
+from bajoo.common.util import xor
 
 
 class TestFuture(object):
@@ -243,3 +245,22 @@ class TestFuture(object):
             f = executor.submit(lambda: 33)
             future = Future.resolve(f)
             assert future.result(1) is 33
+
+
+class TestUtil(object):
+    """Test of the bajoo.common.util module"""
+
+    def test_xor_unicode(self):
+        """Try the xor() function using mix of unicode and bytes inputs."""
+        all_strings = [
+            u'simple unicode string',
+            u'unicode string with unicode charaters: ❄',
+            b'bytes string',
+            b'complex bytes string: \x99 \xff \x00'
+        ]
+        for key, value in product(all_strings, repeat=2):
+            result = xor(value, key)
+            if isinstance(value, bytes):
+                assert xor(result, key) == value
+            else:
+                assert xor(result, key) == value.encode('utf-8')
