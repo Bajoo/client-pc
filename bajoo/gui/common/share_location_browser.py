2f0fd6bde626385fdc4fa76e4648e3e632336c44
Son Huy TRAN <sonhuytran@gmail.com>
2015-10-22 17:35:23 +0200

    Improve share location browser control's events

--- bajoo/gui/common/share_location_browser.py
+++ bajoo/gui/common/share_location_browser.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from os import path
 
+import wx
 from wx.lib.filebrowsebutton import DirBrowseButton
 
 
@@ -13,11 +14,25 @@ class ShareLocationBrowser(DirBrowseButton):
     """
 
     def __init__(self, **kwargs):
-        """Create the DemoPanel."""
-        DirBrowseButton.__init__(self, **kwargs)
+        DirBrowseButton.__init__(
+            self, changeCallback=self._on_changed, **kwargs)
 
         self._share_name = ''
         self._parent_folder = kwargs.get('startDirectory', '.')
+
+    def _on_changed(self, __event):
+        self._post_text_event()
+
+    def _post_text_event(self):
+        """
+        Redirect both events when user clicks button "Browse"
+        or changes the text to the wx.EVT_TEXT event.
+        """
+        evt = wx.CommandEvent(wx.wxEVT_COMMAND_TEXT_UPDATED)
+        evt.SetEventObject(self)
+        evt.SetId(self.GetId())
+
+        wx.PostEvent(self, evt)
 
     def OnBrowse(self, event=None):
         """
@@ -28,10 +43,18 @@ class ShareLocationBrowser(DirBrowseButton):
         self._parent_folder = self.GetValue()
         self.SetValue(path.join(self._parent_folder, self._share_name))
 
-    def set_share_name(self, share_name):
+        self._post_text_event()
+
+    def set_share_name(self, share_name, set_value_now=True):
         """
         Change the share name, this should be called each time user
         changes the share name value.
+
+        Args:
+            set_value_now (boolean):
+                Change the location value immediately
         """
         self._share_name = share_name
-        self.SetValue(path.join(self._parent_folder, self._share_name))
+
+        if set_value_now:
+            self.SetValue(path.join(self._parent_folder, self._share_name))
