441418984f866626ba7a31010942ecfd49434fbf
Kévin Bernard-Allies <kbernard-allies@bajoo.fr>
2015-10-22 10:40:48 +0200

    Add the service Task consumer (module filesync) with unit tests.
    This service will contains the Task queue and execute each task in threads
    dedicated to I/O-bounds operations. This role is actually performs by the main
    filesync module.
    The objectives of this reecriture are:
    - To separate concerns. The  service is fully decoupled to the rest of the
      filesync module (and coukd be easily used by any other producer-consumer
      problem).
    - Make it more easily to write test; And so, write some tests.
    - Moving the filesync module to a code way more readable than what we have now.
    
    TODO:
     - Set a low I/O priority on all task treads.
     - Update filesync to use it.

---
+++ bajoo/filesync/task_consumer.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+
+from collections import deque
+from concurrent.futures import ThreadPoolExecutor
+import logging
+import sys
+from threading import Condition, current_thread
+from ..promise import Promise, is_thenable
+
+_logger = logging.getLogger(__name__)
+
+
+_MAX_WORKER = 5
+_executor = None
+_task_condition = Condition()
+_stop_order = False
+_task_queue = deque()
+
+
+def start():
+    global _executor, _stop_order
+
+    def _clean_and_restart(future):
+        error = future.exception()
+
+        if error:
+            _logger.critical('Filesync task worker has crashed: %s' % error)
+            with _task_condition:
+                if not _stop_order:
+                    future = _executor.submit(_run_worker)
+                    future.add_done_callback(_clean_and_restart)
+
+    _executor = ThreadPoolExecutor(max_workers=_MAX_WORKER)
+    _stop_order = False
+
+    for i in range(_MAX_WORKER):
+        f = _executor.submit(_run_worker, i + 1)
+        f.add_done_callback(_clean_and_restart)
+
+
+def stop():
+    """Stop all operations as soon as possible."""
+    with _task_condition:
+        global _stop_order
+        _stop_order = True
+        _task_condition.notify_all()
+    _executor.shutdown()
+
+
+class Context(object):
+    def __enter__(self):
+        start()
+
+    def __exit__(self, type, value, tb):
+        stop()
+
+
+def add_task(task, priority=False):
+    """Add a task to the list.
+
+    The task is a coroutine who performs IO-bound tasks. It can performs in
+    many "steps", separated by external calls using Promise yielded.
+    The first yielded result who is not a thenable (see promise.is_thenable())
+    is used to resolve the returning promise.
+
+    The initialization phase of the task is executed immediately, in the caller
+    thread. It allow the task to performs init operation sooner (typically, to
+    discard other tasks who are no longer useful).
+    IO-bound operations shouldn't be done before the first yield.
+
+    Each following call to the generator is guaranteed to be executed in the
+    Task consumer context, with thread dedicated to IO-bound operations.
+
+    Args:
+        task (callable): A task is a callable who returns a generator. Each
+            time the generator yield a Promise, the generator is registered to
+            be called as soon as the promise resolve.
+            The task is considered done when it yield a non-Promise value.
+        priority (boolean, optional): if True, the task is set on top of the
+            queue.
+    Returns:
+        Promise: Promise resolved when the task is done. If the task fails
+            (raises an exception), the Promise is rejected with this exception.
+    """
+    def task_executor(resolve, reject):
+        gen = task()
+        try:
+            result = next(gen)
+        except StopIteration:
+            resolve(None)
+        except:
+            reject(*sys.exc_info())
+        else:
+            _call_next_or_set_result(resolve, reject, gen, result, priority)
+
+    return Promise(task_executor)
+
+
+def _iter_generator(resolve, reject, gen, value):
+    """Execute the next step of a task generator."""
+    try:
+        result = gen.send(value)
+    except StopIteration:
+        resolve(value)
+    except:
+        reject(*sys.exc_info())
+    else:
+        _call_next_or_set_result(resolve, reject, gen, result)
+
+
+def _iter_generator_error(resolve, reject, gen, reason):
+    """Execute the next step of a task generator, due to a rejected Promise."""
+    try:
+        result = gen.throw(reason)
+    except StopIteration:
+        resolve(None)
+    except:
+        reject(*sys.exc_info())
+    else:
+        _call_next_or_set_result(resolve, reject, gen, result)
+
+
+def _call_next_or_set_result(resolve, reject, gen, value, priority=True):
+    """When a task has yielded a value, prepare the next step, or resolve.
+
+    If the value yielded is a thenable, then we register the next step in the
+    task queue.
+    Otherwise, the value is the task result, and so the task is fulfilled.
+    """
+    def register_iteration(new_value):
+        with _task_condition:
+            task = (resolve, reject, gen, new_value, False)
+            if priority:
+                _task_queue.appendleft(task)
+            else:
+                _task_queue.append(task)
+            _task_condition.notify()
+
+    def register_iteration_error(reason):
+        with _task_condition:
+            task = (resolve, reject, gen, reason, True)
+            if priority:
+                _task_queue.appendleft(task)
+            else:
+                _task_queue.append(task)
+            _task_condition.notify()
+
+    if is_thenable(value):
+        value.then(register_iteration, register_iteration_error)
+    else:
+        resolve(value)
+        gen.close()
+
+
+def _run_worker(id):
+    """Main loop of the workers (task consumers).
+
+    Each Thread wait for tasks, then execute them and handles theirs yielded
+    promises.
+
+    Args:
+        id (int): number of worker.
+    """
+    current_thread().name = 'Filesync worker %s' % id
+
+    while True:
+        with _task_condition:
+            if _stop_order:
+                return
+
+            try:
+                (resolve, reject,
+                 generator, value, is_error) = _task_queue.popleft()
+            except IndexError:
+                _task_condition.wait()
+                continue
+        if is_error:
+            _iter_generator_error(resolve, reject, generator, value)
+        else:
+            _iter_generator(resolve, reject, generator, value)
---
+++ tests/unit_tests/filesync/task_consumer_test.py
@@ -0,0 +1,222 @@
+# -*- coding:utf-8 -*-
+
+import threading
+import pytest
+
+from bajoo.filesync import task_consumer
+from bajoo.promise import Promise
+
+
+class TestTaskConsumer(object):
+
+    def _make_external_promise(self):
+        """Helper used to make stub Promise.
+
+        Returns:
+            Promise, resolve, reject: the promise and its callbacks.
+        """
+        callbacks = []
+
+        def executor(resolve, reject):
+            callbacks.append(resolve)
+            callbacks.append(reject)
+        return Promise(executor), callbacks[0], callbacks[1]
+
+    def test_add_empty_task(self):
+        """Add a task who is an almost empty generator."""
+
+        with task_consumer.Context():
+            task_executed = []
+
+            def task():
+                task_executed.append(True)
+                yield
+
+            promise = task_consumer.add_task(task)
+            promise.result(0.01)
+            assert task_executed
+
+    def test_add_task_returning_value(self):
+        """Add a simple task who must return a value."""
+        with task_consumer.Context():
+
+            def task():
+                yield 56
+
+            promise = task_consumer.add_task(task)
+            assert promise.result(0.01) is 56
+
+    def test_add_task_multistep(self):
+        """Add a task who has to wait other external tasks (promise)."""
+        p1, resolve, _ = self._make_external_promise()
+        p2, resolve2, _ = self._make_external_promise()
+
+        def task():
+            value = yield p1
+            assert value is 44
+
+            value2 = yield p2
+            yield value2 * 2
+
+        with task_consumer.Context():
+            p_task = task_consumer.add_task(task)
+            resolve(44)
+            resolve2(26)
+            assert p_task.result(0.01) is 52
+
+    def test_all_step_use_dedicated_thread(self):
+        """Ensures the code in a task is always executed in a filesync thread.
+
+        The initialization phase (before the first yield) of a Task is always
+        executed immediately in the caller thread.
+        The main goal is to allow it discarding other stale tasks.
+
+        The rest of the code is always executed in a thread belonging to the
+        filesync threads.
+        """
+        main_thread = threading.current_thread().ident
+        p1, resolve, _ = self._make_external_promise()
+        p2, resolve2, _ = self._make_external_promise()
+
+        def task():
+            assert threading.current_thread().ident is main_thread
+            yield p1
+            assert threading.current_thread().ident is not main_thread
+            yield p2
+            assert threading.current_thread().ident is not main_thread
+            yield Promise.resolve(None)
+            assert threading.current_thread().ident is not main_thread
+
+        with task_consumer.Context():
+            p_task = task_consumer.add_task(task)
+            resolve(None)
+            resolve2(None)
+            p_task.result(0.01)
+
+    def test_add_task_waiting_rejected_promise(self):
+        """Add a task who should fail due to a rejected promise."""
+        class Err(Exception):
+            pass
+
+        def task():
+            yield Promise.resolve('OK')
+            yield Promise.reject(Err())
+
+        with task_consumer.Context():
+            p = task_consumer.add_task(task)
+            with pytest.raises(Err):
+                p.result(0.01)
+
+    def test_add_task_catching_rejected_promise(self):
+        """Add a task who will catch a rejected promise."""
+        class Err(Exception):
+            pass
+
+        def task():
+            yield Promise.resolve('OK')
+            with pytest.raises(Err):
+                yield Promise.reject(Err())
+            yield 'OK'
+
+        with task_consumer.Context():
+            p = task_consumer.add_task(task)
+            assert p.result(0.01) == 'OK'
+
+    def test_add_failing_task(self):
+        """Add a task who will raises an Exception."""
+        class Err(Exception):
+            pass
+
+        def task():
+            yield Promise.resolve(True)
+            raise Err()
+
+        with task_consumer.Context():
+            p = task_consumer.add_task(task)
+
+            with pytest.raises(Err):
+                p.result(0.1)
+
+    def test_add_many_tasks(self):
+        """Add 100 new tasks and wait them all."""
+        promises = []
+
+        def task():
+            yield Promise.resolve(1)
+            yield Promise.resolve(2)
+            yield Promise.resolve(3)
+            yield 1
+        with task_consumer.Context():
+            for i in range(40):
+                promises.append(task_consumer.add_task(task))
+
+            result = Promise.all(promises).result(0.01)
+            print(result)
+
+            assert sum(result) is 40
+
+    def test_add_concurrent_tasks(self):
+        """Add three tasks who are required to run at the same time.
+
+        The task A will wait the Task B, then B will wait A.
+
+        This test "force" the tasks to be executed in a non-linear order.
+
+        """
+        p1_a, r1_a, _ = self._make_external_promise()
+        p1_b, r1_b, _ = self._make_external_promise()
+        p1_c, r1_c, _ = self._make_external_promise()
+        p2_a, r2_a, _ = self._make_external_promise()
+        p2_b, r2_b, _ = self._make_external_promise()
+        p2_c, r2_c, _ = self._make_external_promise()
+
+        def task_A():
+            r1_a(None)
+            yield p1_b
+            r2_a(None)
+            yield p2_c
+            yield 'A'
+
+        def task_B():
+            r1_b(None)
+            yield p1_c
+            r2_b(None)
+            yield p2_a
+            yield 'B'
+
+        def task_C():
+            r1_c(None)
+            yield p1_a
+            r2_c(None)
+            yield p2_b
+            yield 'C'
+
+        with task_consumer.Context():
+            results = Promise.all([
+                task_consumer.add_task(task_A),
+                task_consumer.add_task(task_B),
+                task_consumer.add_task(task_C)
+            ]).result(0.01)
+            assert results == list('ABC')
+
+    def test_ensure_task_generator_are_closed(self):
+        """Ensure the task generators are properly closed after use.
+
+        If a generator has yielded the final result, and the caller don't want
+        to iter until the end, the caller must close the generator.
+        Closing the generator will raise an exception GeneratorExit, and so
+        allow the generator to clean resources.
+        Without the close, resources locked by `with` will not be released.
+        """
+        is_generator_closed = []
+
+        def task():
+            try:
+                yield 'RESULT'
+            except GeneratorExit:
+                is_generator_closed.append(True)
+
+        with task_consumer.Context():
+            p = task_consumer.add_task(task)
+            assert p.result(0.01) == 'RESULT'
+            assert is_generator_closed
