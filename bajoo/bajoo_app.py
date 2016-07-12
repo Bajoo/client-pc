9ab90f2e8acf2591600b1b088b0b5f3365d4f4ae
Kévin Bernard-Allies <kbernard-allies@bajoo.fr>
2016-07-12 15:51:21 +0200

    Changes URL of software updates

--- bajoo/bajoo_app.py
+++ bajoo/bajoo_app.py
@@ -94,7 +94,7 @@ class BajooApp(wx.App):
 
     def __init__(self):
         self._checker = None
-        self._updater = SoftwareUpdater(self, "https://www.bajoo.fr/downloads/")
+        self._updater = SoftwareUpdater(self, "https://www.bajoo.fr/downloads/win32/updates")
         self._home_window = None
         self._main_window = None
         self._about_window = None
