73622e698b54e541c192d2e78996f514cbf2dec8
Kévin Bernard-Allies <kbernard-allies@bajoo.fr>
2016-11-23 16:28:40 +0100

    Replace custom IPC protocol in TaskBarIcon's Unity version, by gtk_process.
    Also, the implementation as been renamed in the more accurate
    `TaskBarIconAppIndicator` name.

--- bajoo/gui/views/__init__.py
+++ bajoo/gui/views/__init__.py
@@ -8,25 +8,29 @@ from ...gtk_process import is_gtk3_process, proxy_factory
 # flake8: noqa
 from .about_window_wx_view import AboutWindowWxView
 from .task_bar_icon_wx_view import TaskBarIconWxView
-from .task_bar_icon_unity_adapter_view import TaskBarIconUnityAdapterView
 
 if is_gtk3_process():
     from .task_bar_icon_gtk_view import TaskBarIconGtkView
+    from .task_bar_icon_appindicator_view import TaskBarIconAppIndicatorView
     from .about_window_gtk_view import AboutWindowGtkView
 else:
-    AboutWindowGtkView = proxy_factory('AboutWindowView', __name__)
-    TaskBarIconGtkView = proxy_factory('TaskBarIconView', __name__)
+    AboutWindowGtkView = proxy_factory('AboutWindowGtkView', __name__)
+    TaskBarIconGtkView = proxy_factory('TaskBarIconGtkView', __name__)
+    TaskBarIconAppIndicatorView = proxy_factory('TaskBarIconAppIndicatorView', __name__)
+
+
+def _get_task_bar_icon_view():
+    # Special case for unity desktop
+    if (sys.platform not in ["win32", "cygwin", "darwin"] and
+            os.environ.get('DESKTOP_SESSION', '').startswith("ubuntu")):
+        return TaskBarIconAppIndicatorView
+    else:
+        return TaskBarIconWxView
 
 
 # Choices of view implementation
 AboutWindowView = AboutWindowWxView
-
-
-TaskBarIconView = TaskBarIconWxView
-if (sys.platform not in ["win32", "cygwin", "darwin"] and
-        os.environ.get('DESKTOP_SESSION', '').startswith("ubuntu")):
-    # special case for Unity desktop
-    TaskBarIconView = TaskBarIconUnityAdapterView
+TaskBarIconView = _get_task_bar_icon_view()
 
 
 __all__ = [
---
+++ bajoo/gui/views/task_bar_icon_appindicator_view.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+
+from os.path import abspath
+from functools import partial
+
+from ...app_status import AppStatus
+from .task_bar_icon_base_view import (app_status_to_icon_files, MenuEntry,
+                                      TaskBarIconAction, TaskBarIconBaseView)
+
+import gi
+gi.require_version('AppIndicator3', '0.1')
+from gi.repository import AppIndicator3, GObject, Gtk  # noqa
+
+
+APPINDICATOR_ID = 'bajoo'
+
+
+class TaskBarIconAppIndicatorView(TaskBarIconBaseView):
+    """Implementation of task bar icon using libappindicator.
+
+    Notes:
+        The menu is build once, then reused at each click on the trayicon
+        (by opposite to other libraries like wxPython who rebuild the menu at
+        each display). We rebuild the menu only on explicit changes.
+
+    """
+    def __init__(self, ctrl):
+        TaskBarIconBaseView.__init__(self, ctrl)
+        self.indicator = AppIndicator3.Indicator.new(
+            APPINDICATOR_ID,
+            app_status_to_icon_files[AppStatus.NOT_CONNECTED],
+            AppIndicator3.IndicatorCategory.SYSTEM_SERVICES)
+        self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+
+        self._is_connected = False
+        self._container_status_list = []
+        self.set_app_status(AppStatus.NOT_CONNECTED)
+
+    def set_app_status(self, app_status):
+        self._is_connected = app_status != AppStatus.NOT_CONNECTED
+        icon_path = abspath(app_status_to_icon_files[app_status])
+        self.indicator.set_icon(icon_path)
+        self.build_menu()
+
+    def notify_lang_change(self):
+        self.build_menu()
+
+    def set_container_status_list(self, status_list):
+        self._container_status_list = status_list
+        self.build_menu()
+
+    def _inner_build_menu(self, items_list):
+        gtk_menu = Gtk.Menu()
+        for m in items_list:
+            if m is MenuEntry.Separator:
+                gtk_menu.append(Gtk.SeparatorMenuItem())
+                continue
+
+            if m.icon is None:
+                menu_item = Gtk.MenuItem(m.title)
+            else:
+                menu_item = Gtk.ImageMenuItem()
+                menu_item.set_label(m.title)
+
+                # TODO: try to reuse Image !!!
+                img = Gtk.Image()
+                img.set_from_file(m.icon)
+                menu_item.set_image(img)
+                menu_item.set_always_show_image(True)
+
+            if m.action:
+                menu_item.connect('activate',
+                                  partial(self.clic_event, m.action, m.target))
+
+            if m.children:
+                sub_gtk_menu = self._inner_build_menu(m.children)
+                menu_item.set_submenu(sub_gtk_menu)
+
+            if not m.enabled:
+                menu_item.set_sensitive(False)
+
+            gtk_menu.append(menu_item)
+
+        return gtk_menu
+
+    def build_menu(self):
+        items_list = MenuEntry.make_menu(self._is_connected,
+                                         self._container_status_list)
+        gtk_menu = self._inner_build_menu(items_list)
+        gtk_menu.show_all()
+        self.indicator.set_menu(gtk_menu)
+
+    def clic_event(self, action, target, _event):
+        """called when a menu action is triggered"""
+
+        if action == TaskBarIconAction.OPEN_CONTAINER:
+            self.controller.open_container_action(target)
+        elif action == TaskBarIconAction.NAVIGATE:
+            self.controller.navigate_action(target)
+        elif action == TaskBarIconAction.EXIT:
+            self.controller.exit_action()
+        return False
+
+
+def main():
+    import logging
+    from ...app_status import AppStatus
+    from ..enums import ContainerStatus
+    logging.basicConfig()
+
+    class Controller(object):
+        def __init__(self, View):
+            self.view = View(self)
+            self.view.set_app_status(AppStatus.NOT_CONNECTED)
+            self.view.set_container_status_list([
+                ('MyBajoo', '.', ContainerStatus.SYNC_DONE),
+                ('Container tests', './tests', ContainerStatus.SYNC_PROGRESS),
+                ('Container #2', '/container-2', ContainerStatus.SYNC_STOP)
+            ])
+            print('ok')
+
+        def primary_action(self):
+            print('Execute primary action')
+
+        def navigate_action(self, destination):
+            print('Navigate to "%s"' % destination)
+            self.view.set_app_status(AppStatus.SYNC_DONE)
+
+        def open_container_action(self, container_path):
+            print('Open container at "%s"' % container_path)
+            self.view.set_app_status(AppStatus.SYNC_DONE)
+
+        def exit_action(self):
+            print('Exit ...')
+            self.view.destroy()
+            Gtk.main_quit()
+
+    Controller(TaskBarIconAppIndicatorView)
+    Gtk.main()
+
+if __name__ == '__main__':
+    main()
